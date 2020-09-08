03af84dca31f6ac7e93e27e97b56113886fbc1d7
DeveshChau <deveshachaudhari@gmail.com>
2020-09-08 12:40:41 +0530

    checkLength of input

--- form-validator/script.js
+++ form-validator/script.js
@@ -4,16 +4,6 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-function checkRequired(input) {
-    input.forEach(element => {
-        if (element.value.trim() === '') {
-            showError(element, `${getElement(element)} is required`);
-        } else {
-            showSuccess(element)
-        } 
-    });    
-}
-
 function showError(input, message) {
     const parentEle = input.parentElement;
     parentEle.className = 'form-control error';
@@ -29,7 +19,29 @@ function showSuccess(input) {
 function getElement(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
+function checkRequired(input) {
+    input.forEach(element => {
+        if (element.value.trim() === '') {
+            showError(element, `${getElement(element)} is required`);
+        } else {
+            showSuccess(element)
+        } 
+    });    
+}
+
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(input, `${getElement(input)} must be more than ${min}`);
+	} else if (input.value.length > max) {
+		showError(input, `${getElement(input)} must be less than ${max}`);		
+	} else {
+		showSuccess(input);
+	}
+}
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    checkRequired([username, email, password, password2]);
+	checkRequired([username, email, password, password2]);
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
 })
