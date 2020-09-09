23173f7eab7d987aeccc0e26f6f68fb91bee5630
DeveshChau <deveshachaudhari@gmail.com>
2020-09-09 12:19:07 +0530

    store data in local storage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -2,13 +2,21 @@ const container = document.querySelector(".container");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieTicket = document.getElementById("movie");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 let ticketPrice = +movieTicket.value;
 
 function updateSelectedCount() {
   const seatSelectesd = document.querySelectorAll(".row .seat.selected");
+  const seatsIndex = [...seatSelectesd].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
   const selectedCount = seatSelectesd.length;
   count.innerText = selectedCount;
   total.innerText = selectedCount * ticketPrice;
+}
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
 container.addEventListener("click", (e) => {
@@ -23,5 +31,6 @@ container.addEventListener("click", (e) => {
 
 movieTicket.addEventListener("change", (e) => {
   ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
